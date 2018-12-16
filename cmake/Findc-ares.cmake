a604be5144ae81b54489d5f1ad89d7aa2eeb4188
Kefu Chai <kchai@redhat.com>
2018-12-16 22:47:09 +0800

    build: add CARES_ROOT_DIR option for Findc-ares.cmake
    so we can use c-ares library installed into non-standard directory
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- cmake/Findc-ares.cmake
+++ cmake/Findc-ares.cmake
@@ -1,55 +1,36 @@
-#
-# This file is open source software, licensed to you under the terms
-# of the Apache License, Version 2.0 (the "License").  See the NOTICE file
-# distributed with this work for additional information regarding copyright
-# ownership.  You may not use this file except in compliance with the License.
-#
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
+find_package(PkgConfig QUIET)
 
-#
-# Copyright (C) 2018 Scylladb, Ltd.
-#
+pkg_search_module(_cares
+  libcares)
 
-find_path (c-ares_INCLUDE_DIR
-  NAMES ares_dns.h)
+set(_cares_ROOT_HINTS
+  ${CARES_ROOT_DIR}
+  $ENV{CARES_ROOT_DIR})
 
-find_library (c-ares_LIBRARY
-  NAMES cares)
+find_path(c-ares_INCLUDE_DIR
+  NAMES ares_dns.h
+  HINTS ${_cares_INCLUDE_DIRS} ${_cares_ROOT_HINTS}
+  PATH_SUFFIXES include)
 
-if (c-ares_INCLUDE_DIR AND c-ares_LIBRARY)
-  set (c-ares_FOUND ON)
-  set (c-ares_LIBRARIES ${c-ares_LIBRARY})
-  set (c-ares_INCLUDE_DIRS ${c-ares_INCLUDE_DIR})
-endif ()
+find_library(c-ares_LIBRARY
+  NAMES cares
+  HINTS ${_cares_LIBRARY_DIRS} ${_cares_ROOT_HINTS}
+  PATH_SUFFIXES lib/${CMAKE_LIBRARY_ARCHITECTURE} lib)
 
-include (FindPackageHandleStandardArgs)
+set(c-ares_VERSION ${_cares_VERSION})
 
-find_package_handle_standard_args (c-ares
-  FOUND_VAR c-ares_FOUND
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(c-ares
   REQUIRED_VARS
     c-ares_INCLUDE_DIR
     c-ares_LIBRARY
   VERSION_VAR c-ares_VERSION)
 
-if (c-ares_FOUND AND NOT (TARGET c-ares::c-ares))
-  add_library (c-ares::c-ares UNKNOWN IMPORTED)
-
-  set_target_properties (c-ares::c-ares
-    PROPERTIES
-      IMPORTED_LOCATION ${c-ares_LIBRARY}
-      INTERFACE_INCLUDE_DIRECTORIES ${c-ares_INCLUDE_DIRS})
-endif ()
-
-mark_as_advanced (
-  c-ares_INCLUDE_DIR
-  c-ares_LIBRARY)
+if(c-ares_FOUND AND NOT (TARGET c-ares::c-ares))
+  add_library(c-ares::c-ares UNKNOWN IMPORTED)
+  set_target_properties(c-ares::c-ares PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${c-ares_INCLUDE_DIR}"
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${c-ares_LIBRARY}")
+endif()
