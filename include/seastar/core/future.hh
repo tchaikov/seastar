10bd95ff4fb02dbd63e7ff1e609a293f20c36003
Matan Breizman <mbreizma@redhat.com>
2024-02-04 10:31:49 +0000

    include/seastar/core/future.hh: Keep aa4a648733d
    ```
    Revert "future: add concept constraints to handle_exception()"
    Ceph is stilling using errorated future with handle_exception(), but
    errorated future is not a seastar::Futuer<>. so we need to revert this
    change at this moment.
    
    This reverts commit 955c584.
    ```
    
    See: https://github.com/ceph/seastar/commit/aa4a648733d6da22ad2a49d71b66cf1896ccc8fb
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>
    (cherry picked from commit 0292cd876a038de4040d049dee1e63c338485fa7)

--- include/seastar/core/future.hh
+++ include/seastar/core/future.hh
@@ -1949,10 +1949,12 @@ public:
     /// successful value; Because handle_exception() is used here on a
     /// future<>, the handler function does not need to return anything.
     template <typename Func>
+    /* Broken?
     requires std::is_invocable_r_v<future<T> ,Func, std::exception_ptr>
                     || (std::tuple_size_v<tuple_type> == 0 && std::is_invocable_r_v<void, Func, std::exception_ptr>)
                     || (std::tuple_size_v<tuple_type> == 1 && std::is_invocable_r_v<T, Func, std::exception_ptr>)
                     || (std::tuple_size_v<tuple_type> > 1 && std::is_invocable_r_v<tuple_type ,Func, std::exception_ptr>)
+    */
     future<T> handle_exception(Func&& func, std::source_location sl = std::source_location::current()) noexcept {
       // Avoid having to special-case lvalue-references downstream by converting
       // them to an rvalue reference here.
